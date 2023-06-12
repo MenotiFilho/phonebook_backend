9653d4a435671e4f3329b09ebc610726cea7c1da
Menoti Filho <menotimfilho@gmail.com>
2023-06-12 17:01:11 -0300

    final

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-underscore-dangle */
 const express = require('express');
+
 const app = express();
 const cors = require('cors');
 const morgan = require('morgan');
 const {
-	connectToDatabase,
-	getAllPersons,
-	addPerson,
-	deletePerson,
-	Person,
+  connectToDatabase,
+  getAllPersons,
+  addPerson,
+  deletePerson,
+  Person,
 } = require('./mongo');
 require('dotenv').config();
 
@@ -16,141 +19,139 @@ app.use(express.json());
 app.use(express.static('dist'));
 app.use(cors());
 app.use(
-	morgan('tiny', {
-		skip: (req) => req.method === 'POST',
-	})
+  morgan('tiny', {
+    skip: (req) => req.method === 'POST',
+  }),
 );
 
 app.use((req, res, next) => {
-	if (req.method === 'POST') {
-		morgan(
-			':method :url :status :res[content-length] - :response-time ms :post-data'
-		)(req, res, next);
-	} else {
-		next();
-	}
+  if (req.method === 'POST') {
+    morgan(
+      ':method :url :status :res[content-length] - :response-time ms :post-data',
+    )(req, res, next);
+  } else {
+    next();
+  }
 });
 
-morgan.token('post-data', (req) => {
-	return JSON.stringify(req.body);
-});
+morgan.token('post-data', (req) => JSON.stringify(req.body));
 
 // Error handling middleware
 const errorHandler = (err, req, res, next) => {
-	console.error('Error:', err);
+  console.error('Error:', err);
 
-	let statusCode = 500;
-	let errorMessage = 'Internal Server Error';
+  let statusCode = 500;
+  let errorMessage = 'Internal Server Error';
 
-	if (err.statusCode) {
-		statusCode = err.statusCode;
-		errorMessage = err.message;
-	}
+  if (err.statusCode) {
+    statusCode = err.statusCode;
+    errorMessage = err.message;
+  }
 
-	res.status(statusCode).json({ error: errorMessage });
+  res.status(statusCode).json({ error: errorMessage });
 };
 
 app.use(errorHandler);
 
 // Routes
 app.get('/', (req, res) => {
-	res.send('<h1>Hello World!</h1>');
+  res.send('<h1>Hello World!</h1>');
 });
 
 app.get('/api/persons', (req, res, next) => {
-	getAllPersons()
-		.then((persons) => {
-			res.json(persons);
-		})
-		.catch((error) => {
-			next(error);
-		});
+  getAllPersons()
+    .then((persons) => {
+      res.json(persons);
+    })
+    .catch((error) => {
+      next(error);
+    });
 });
 
 app.get('/api/persons/:_id', (req, res, next) => {
-	const id = req.params._id;
+  const id = req.params._id;
 
-	Person.findById(id)
-		.then((person) => {
-			if (person) {
-				res.json(person);
-			} else {
-				const error = new Error('Person not found');
-				error.statusCode = 404;
-				throw error;
-			}
-		})
-		.catch((error) => {
-			next(error);
-		});
+  Person.findById(id)
+    .then((person) => {
+      if (person) {
+        res.json(person);
+      } else {
+        const error = new Error('Person not found');
+        error.statusCode = 404;
+        throw error;
+      }
+    })
+    .catch((error) => {
+      next(error);
+    });
 });
 
 app.delete('/api/persons/:_id', (req, res, next) => {
-	const id = req.params._id;
+  const id = req.params._id;
 
-	deletePerson(id)
-		.then(() => {
-			res.status(204).end();
-		})
-		.catch((error) => {
-			next(error);
-		});
+  deletePerson(id)
+    .then(() => {
+      res.status(204).end();
+    })
+    .catch((error) => {
+      next(error);
+    });
 });
 
 app.get('/info', (req, res, next) => {
-	Person.countDocuments()
-		.then((count) => {
-			const info = {
-				totalPersons: count,
-				timestamp: new Date(),
-			};
-			res.json(info);
-		})
-		.catch((error) => {
-			next(error);
-		});
+  Person.countDocuments()
+    .then((count) => {
+      const info = {
+        totalPersons: count,
+        timestamp: new Date(),
+      };
+      res.json(info);
+    })
+    .catch((error) => {
+      next(error);
+    });
 });
 
 app.put('/api/persons/:_id', (req, res, next) => {
-	const id = req.params._id;
-	const { phone } = req.body;
+  const id = req.params._id;
+  const { phone } = req.body;
 
-	Person.findByIdAndUpdate(id, { phone }, { new: true })
-		.then((updatedPerson) => {
-			if (updatedPerson) {
-				res.json(updatedPerson);
-			} else {
-				const error = new Error('Person not found');
-				error.statusCode = 404;
-				throw error;
-			}
-		})
-		.catch((error) => {
-			next(error);
-		});
+  Person.findByIdAndUpdate(id, { phone }, { new: true })
+    .then((updatedPerson) => {
+      if (updatedPerson) {
+        res.json(updatedPerson);
+      } else {
+        const error = new Error('Person not found');
+        error.statusCode = 404;
+        throw error;
+      }
+    })
+    .catch((error) => {
+      next(error);
+    });
 });
 
 app.post('/api/persons', (req, res, next) => {
-	const { name, phone } = req.body;
+  const { name, phone } = req.body;
 
-	addPerson(name, phone)
-		.then((person) => {
-			res.json(person);
-		})
-		.catch((error) => {
-			next(error);
-		});
+  addPerson(name, phone)
+    .then((person) => {
+      res.json(person);
+    })
+    .catch((error) => {
+      next(error);
+    });
 });
 
 // Start the server
 const PORT = process.env.PORT || 3001;
 
 connectToDatabase()
-	.then(() => {
-		app.listen(PORT, () => {
-			console.log(`Server running on port ${PORT}`);
-		});
-	})
-	.catch((error) => {
-		console.error('Error connecting to the database:', error);
-	});
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error('Error connecting to the database:', error);
+  });
--- mongo.js
+++ mongo.js
@@ -1,51 +1,49 @@
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable no-param-reassign */
 const mongoose = require('mongoose');
 require('dotenv').config();
 
 const connectToDatabase = () => {
-	const url = `mongodb+srv://menotimfilho:${process.env.MONGODB_PASSWORD}@clusterlista.7u18oph.mongodb.net/phonebook?retryWrites=true&w=majority`;
+  const url = `mongodb+srv://menotimfilho:${process.env.MONGODB_PASSWORD}@clusterlista.7u18oph.mongodb.net/phonebook?retryWrites=true&w=majority`;
 
-	return mongoose.connect(url, {
-		useNewUrlParser: true,
-		useUnifiedTopology: true,
-	});
+  return mongoose.connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
 };
 
 const personSchema = new mongoose.Schema({
-	name: String,
-	phone: String,
+  name: String,
+  phone: String,
 });
 
 personSchema.set('toJSON', {
-	transform: (document, returnedObject) => {
-		returnedObject.id = returnedObject._id.toString();
-		delete returnedObject._id;
-		delete returnedObject.__v;
-	},
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
 });
 
 const Person = mongoose.model('Person', personSchema);
 
-const getAllPersons = () => {
-	return Person.find({});
-};
+const getAllPersons = () => Person.find({});
 
 const addPerson = (name, phone) => {
-	const person = new Person({
-		name: name,
-		phone: phone,
-	});
+  const person = new Person({
+    name,
+    phone,
+  });
 
-	return person.save();
+  return person.save();
 };
 
-const deletePerson = (id) => {
-	return Person.findByIdAndDelete(id);
-};
+const deletePerson = (id) => Person.findByIdAndDelete(id);
 
 module.exports = {
-	connectToDatabase,
-	getAllPersons,
-	addPerson,
-	deletePerson,
-	Person,
+  connectToDatabase,
+  getAllPersons,
+  addPerson,
+  deletePerson,
+  Person,
 };
